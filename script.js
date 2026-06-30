32f4d8beaca92c3f80533b48227324c8f23b24f5
SHAHIDDIN SHAIK <mrshyam0923@gmail.com>
2026-06-30 11:21:08 +0530

    Add login button redirect functionality

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const button = document.getElementById("btn");
 button.addEventListener("click", function () {
     alert("Welcome to my Portfolio!");
 });
+
+// Redirect to login page
+const loginBtn = document.getElementById("loginBtn");
+
+if (loginBtn) {
+    loginBtn.addEventListener("click", function () {
+        window.location.href = "login.html";
+    });
+}
